0ba99e9024064ab28405c32d251da60f0393aaa4
paul garcia <pbrain19@gmail.com>
2024-06-13 15:01:36 -0500

    fixes to work locally

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,18 +1,27 @@
 "use client";
 import React from "react";
-import { ConnectButton as ThirdwebConnectButton, useActiveWalletChain } from "thirdweb/react";
+import {
+  ConnectButton as ThirdwebConnectButton,
+  useActiveWalletChain,
+} from "thirdweb/react";
 import { createWallet } from "thirdweb/wallets";
 import client from "@/lib/thirdweb-client";
-import anvil from "@/lib/anvil";
+import { optimism } from "thirdweb/chains";
 
 export default function ConnectButton() {
-    const wallets = [
-        createWallet("io.metamask"),
-        createWallet("com.coinbase.wallet"),
-        createWallet("me.rainbow"),
-    ];
+  const wallets = [
+    createWallet("io.metamask"),
+    createWallet("com.coinbase.wallet"),
+    createWallet("me.rainbow"),
+  ];
 
-    return (
-        <ThirdwebConnectButton connectButton={{ className: "w-full" }} theme="light" chain={anvil} client={client} wallets={wallets} />
-    )
+  return (
+    <ThirdwebConnectButton
+      connectButton={{ className: "w-full" }}
+      theme="light"
+      chain={optimism}
+      client={client}
+      wallets={wallets}
+    />
+  );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -2,29 +2,32 @@ import getContract from "./lib/get-contract";
 import Token from "./types/token";
 
 export const tokens: { [id: string]: Token } = {
-  "weth": {
-    address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  weth: {
+    address: "0x4200000000000000000000000000000000000006",
     symbol: "WETH",
     decimals: 18,
-    image: "https://assets.coingecko.com/coins/images/2518/standard/weth.png?1696503332"
+    image:
+      "https://assets.coingecko.com/coins/images/2518/standard/weth.png?1696503332",
   },
-  "usdc": {
-    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+  usdc: {
+    address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
     symbol: "USDC",
     decimals: 6,
-    image: "https://assets.coingecko.com/coins/images/6319/standard/usdc.png?1696506694"
+    image:
+      "https://assets.coingecko.com/coins/images/6319/standard/usdc.png?1696506694",
   },
-  "usdt": {
-    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+  usdt: {
+    address: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
     symbol: "USDT",
     decimals: 6,
-    image: "https://assets.coingecko.com/coins/images/325/standard/Tether.png?1696501661"
-  }
-}
+    image:
+      "https://assets.coingecko.com/coins/images/325/standard/Tether.png?1696501661",
+  },
+};
 
 export const ROUTER = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 export const FACTORY = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
-export const QUOTER = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
+export const QUOTER = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
 
 export const ROUTER_CONTRACT = getContract({ address: ROUTER });
 export const FACTORY_CONTRACT = getContract({ address: FACTORY });
